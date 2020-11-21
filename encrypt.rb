3fc97428b68aa61f5ed15165ffad76e19a2b5ebc
ProhorA8 <Procustom4@gmail.com>
2020-11-21 21:22:51 +0300

    sha2

--- encrypt.rb
+++ encrypt.rb
@@ -1,14 +1,14 @@
-# Задача 40-1 — Шифрование MD5, SHA1
+# Задача 40-1 — Шифрование MD5, SHA1, SHA2
 require 'digest'
 
 puts "Введите слово или фразу для шифрования:"
 user_input = STDIN.gets.chomp
 
-puts "Каким способом зашифровать:\n1. MD5 \n2. SHA1"
+puts "Каким способом зашифровать:\n1. MD5 \n2. SHA1 \n3. SHA2"
 choise = STDIN.gets.to_i
 
-until choise.between?(1, 2)
-  puts "Выберите 1 или 2"
+until choise.between?(1, 2, 3)
+  puts "Выберите 1, 2 или 3"
   choise = STDIN.gets.to_i
 end
 
@@ -17,4 +17,5 @@ puts "Вот что получилось:"
 case choise
 when 1 then puts Digest::MD5.hexdigest(user_input)
 when 2 then puts Digest::SHA1.hexdigest(user_input)
+when 3 then puts Digest::SHA2.hexdigest(user_input)
 end
